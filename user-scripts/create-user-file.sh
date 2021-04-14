8b55a37fc14bd738f63a42f9a87763f59fd9e926
luza-onarev <luza.onarev@protonmail.com>
2021-04-14 13:16:05 +0200

    Update create-user-file.sh

--- user-scripts/create-user-file.sh
+++ user-scripts/create-user-file.sh
@@ -16,28 +16,28 @@ if [[ $(find /var/www/html/users/ -type f | wc -l) != 0 ]]; then
 			echo "[-] $(display_date) ===== CREATION OF USER $username =====" |& tee -a "$log_file"
 
 			if sudo useradd -b /home/"$username" -d /home/"$username" -m -s /bin/false "$username"; then
-				echo "[-] $(display_date) - creating $username ..." |& tee -a "$log_file"
+				echo "[-] $(display_date) - Creating $username ..." |& tee -a "$log_file"
 
 				# sets password
-				echo "[-] $(display_date) - changing $username's password ..." |& tee -a "$log_file"
+				echo "[-] $(display_date) - Changing $username's password ..." |& tee -a "$log_file"
 				userch="$username"
 				passch="$password"
 				echo "$userch:$passch" | sudo chpasswd
 
 				# changes /home owner and permissions
-				echo "[-] $(display_date) - changing /home/$username owner to $username:$username ..." |& tee -a "$log_file"
+				echo "[-] $(display_date) - Changing /home/$username owner to $username:$username ..." |& tee -a "$log_file"
 				sudo chown -R "$username":"$username" /home/"$username"
 
-				echo "[-] $(display_date) - changing /home/$username permissions to 700 ..." |& tee -a "$log_file"
+				echo "[-] $(display_date) - Changing /home/$username permissions to 700 ..." |& tee -a "$log_file"
 				sudo chmod -R 700 /home/"$username"
 
 				# looks for the user in the /etc/passwd to check if it's created
 				if getent passwd | grep "\\<$username\\>"; then
-					echo "[+] $(display_date) - user $username was created successfully" |& tee -a "$log_file"
+					echo "[+] $(display_date) - User $username was created successfully" |& tee -a "$log_file"
 				fi
 			else
 				# user creation fails
-				echo "[!] $(display_date) - user creation failed" |& tee -a "$log_file"
+				echo "[!] $(display_date) - User creation failed" |& tee -a "$log_file"
 			fi
 
 			sudo rm -rf "$file"
