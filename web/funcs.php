f011e8af58e6d2116de80ddcfa21a271a928e1d0
luza-onarev <luza.onarev@protonmail.com>
2021-04-14 11:56:45 +0200

    Update funcs.php

--- web/funcs.php
+++ web/funcs.php
@@ -1,14 +1,14 @@
 <?php
-	// function to validate the user info before insert it to database //
+	## function to validate the user info before insert it to database ##
 	function validate_signup_info() {
-		// variables to use in inserts ands selects
+		# variables to use in inserts and selects
 		$username = $_POST["username"];
-	    $email = $_POST["email"];
-	    $password = hash('sha256', $_POST["password1"]);
-	    $date = strftime("%Y-%m-%d"); //yyyy-mm-dd
+		$email = $_POST["email"];
+		$password = hash('sha256', $_POST["password1"]);
+		$date = strftime("%Y-%m-%d"); //yyyy-mm-dd
 
-	    // quaries to check if username already exists
-	    $check_mail = mysqli_query($GLOBALS["db_conn"], "SELECT * FROM user WHERE email = '$email'");
+		# queries to check if all values are correct or user already exists
+		$check_mail = mysqli_query($GLOBALS["db_conn"], "SELECT * FROM user WHERE email = '$email'");
 		$check_username = mysqli_query($GLOBALS["db_conn"], "SELECT * FROM user WHERE username = '$username'");
 
 		$valid_signup = 1;
@@ -17,62 +17,74 @@
 
 		if ($username != $_POST["username"]) {
 			echo '<div class="error-signup">';
-		    echo "Invalid characters found in user name!<br>";
-		    echo "Valid characters: a to z and 0 to 9<br>";
-		    echo "</div>";
-		    $valid_signup = 0;
+			echo "Invalid characters found in user name!<br>";
+			echo "Valid characters: a to z and 0 to 9<br>";
+			echo "</div>";
+			$valid_signup = 0;
 		}
 
 		if (mysqli_num_rows($check_username) > 0) {
 			echo '<div class="error-signup">';
-		    echo "User name already exists!";
-		    echo "</div>";
-		    $valid_signup = 0;
+			echo "User name already exists!";
+			echo "</div>";
+			$valid_signup = 0;
 		}
 
 		if (mysqli_num_rows($check_mail) > 0) {
 			echo '<div class="error-signup">';
-		    echo "Email account already exists!";
-		    echo "</div>";
-		    $valid_signup = 0;
+			echo "Email account already exists!";
+			echo "</div>";
+			$valid_signup = 0;
 		}
 
 		if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
 			echo '<div class="error-signup">';
-		    echo "Email address seems bad formed!";
-		    echo "</div>";
-		    $valid_signup = 0;
+			echo "Email address seems bad formed!";
+			echo "</div>";
+			$valid_signup = 0;
 		}
 
 		if ($_POST["password1"] != $_POST["password2"]) {
 			echo '<div class="error-signup">';
-		    echo "Passwords don't match!";
-		    echo "</div>";
-		    $valid_signup = 0;
+			echo "Passwords don't match!";
+			echo "</div>";
+			$valid_signup = 0;
 		}		
 
+		# users doesn't exist and it's good formatted
 		if ($valid_signup) {
-		    $insert_sentence = "INSERT INTO `user` (`email`, `username`, `password`, `creation_date`) VALUES ('$email', '$username', '$password', '$date');";
-		    $insert_data = mysqli_query($GLOBALS["db_conn"], $insert_sentence);
-		    create_user_db();
+			$insert_sentence = "INSERT INTO `user` (`email`, `username`, `password`, `creation_date`) VALUES ('$email', '$username', '$password', '$date');";
+			$insert_data = mysqli_query($GLOBALS["db_conn"], $insert_sentence);
+			create_user_db();
 
-		    if ($insert_data) {
-		    	echo '<div class="succ-signup">';
-			    echo "The user was created successfully!<br>";
-			    echo "Access your control panel <a href='/home'>here</a>!";
-			    echo "</div>";
+			if ($insert_data) {
+				# create file to create local user
+				$raw_password = $_POST["password1"];
+				$userfile = "username = $username\n";
+				$passfile = "password = $raw_password\n";
+				
+				$file = fopen($_SERVER['DOCUMENT_ROOT'] . "/users/$username","wb");
+				fwrite($file, $userfile);
+				fwrite($file, $passfile);
+				fclose($file);
 
-			    return 1;
-		    } else {
-		    	echo mysqli_error($GLOBALS["db_conn"]);
-		    }
+				# display successful message 
+				echo '<div class="succ-signup">';
+				echo "The user was created successfully!<br>";
+				echo "Access your control panel <a href='/home'>here</a>!";
+				echo "</div>";
+
+				return 1;
+			} else {
+				echo mysqli_error($GLOBALS["db_conn"]);
+			}
 		}
 
 		mysqli_close($GLOBALS["db_conn"]);
 		$_POST = array();
 	}
 
-	// function to login //
+	## function to validate login ##
 	function validate_login_info() {
 		$valid_login = 1;
 
@@ -81,10 +93,10 @@
 
 		if ($username != $_POST["username"]) {
 			echo '<div class="error-signup">';
-		    echo "Invalid characters found in user name!<br>";
-		    echo "Valid characters: a to z and 0 to 9<br>";
-		    echo "</div>";
-		    $valid_login = 0;
+			echo "Invalid characters found in user name!<br>";
+			echo "Valid characters: a to z and 0 to 9<br>";
+			echo "</div>";
+			$valid_login = 0;
 		} else {
 			$check_username = mysqli_query($GLOBALS["db_conn"], "SELECT username FROM user WHERE username = '$username'");
 			$check_password = mysqli_query($GLOBALS["db_conn"], "SELECT password FROM user WHERE username = '$username'");
@@ -94,14 +106,14 @@
 
 			if (is_null($check_username_data)) {
 				echo '<div class="error-signup">';
-			    echo "User name not found!<br>";
-			    echo "</div>";
-			    $valid_login = 0;
+				echo "User name not found!<br>";
+				echo "</div>";
+				$valid_login = 0;
 			} elseif ($check_password_data["password"] != $password) {
 				echo '<div class="error-signup">';
-			    echo "Wrong password!<br>";
-			    echo "</div>";
-			    $valid_login = 0;
+				echo "Wrong password!<br>";
+				echo "</div>";
+				$valid_login = 0;
 			} else {
 				$valid_login = 1;
 			}
@@ -127,16 +139,16 @@
 
 		if ($check_curr_password_data["password"] != "$curr_hash_password") {
 			echo '<div class="error-signup">';
-		    echo "Wrong current password!<br>";
-		    echo "</div>";
-		    $change_password = 0;
+			echo "Wrong current password!<br>";
+			echo "</div>";
+			$change_password = 0;
 		}
 
 		if ($new_password != $new_password2) {
 			echo '<div class="error-signup">';
-		    echo "New passwords don't match!<br>";
-		    echo "</div>";
-		    $change_password = 0;
+			echo "New passwords don't match!<br>";
+			echo "</div>";
+			$change_password = 0;
 		}
 
 		if ($change_password) {
@@ -145,7 +157,7 @@
 			$change_password_command = mysqli_query($GLOBALS["db_conn"], "UPDATE user SET password = '$new_hash_password' WHERE username = '$username'");
 
 			if ($change_password_command) {
-			    return 1;
+				return 1;
 			}
 		}
 
