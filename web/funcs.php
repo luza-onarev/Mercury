5514d31713b03f7afcff813269894f34b44a9e62
luza-onarev <luza.onarev@protonmail.com>
2021-05-07 13:58:17 +0200

    Update funcs.php

--- web/funcs.php
+++ web/funcs.php
@@ -241,7 +241,7 @@
 			"CREATE DATABASE `$db_name` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;",
 			"CREATE USER '$db_user'@'%' IDENTIFIED BY PASSWORD('$db_password');",
 			"GRANT USAGE ON $db_name.* TO '$db_user'@'%' IDENTIFIED BY '$db_password' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;",
-			"GRANT SELECT , INSERT , UPDATE, DELETE, CREATE , DROP ON `$db_name`.* TO '$db_user'@'%';",
+			"GRANT SELECT , INSERT , UPDATE, DELETE, CREATE , ALTER , DROP ON `$db_name`.* TO '$db_user'@'%';",
 			"FLUSH PRIVILEGES;"
 		);
 
