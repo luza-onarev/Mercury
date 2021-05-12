006ea9a617e59aa5e3b290ddcdb36e78e9e30caf
luza-onarev <luza.onarev@protonmail.com>
2021-05-12 13:28:31 +0200

    Update ssh_users.sh

--- user-scripts/ssh_users.sh
+++ user-scripts/ssh_users.sh
@@ -6,18 +6,17 @@ display_date() {
 }
 
 log_file="/etc/user-scripts/log/ssh_users.log"
-sql_pass=$(sudo cat /etc/sql_pass)
+sql_pass=$(sudo cat /etc/sql_login_user_pass)
 
-for db_user in $(echo "SELECT username FROM user;" | mysql --user=root -p"$sql_pass" users | tail -n +2); do
-	if [[ $(echo "SELECT is_premium FROM user WHERE username='$db_user';" | mysql --user=root -p"$sql_pass" users | tail -n +2) == 1 ]]; then
+for db_user in $(echo "SELECT username FROM user;" | mysql --user=login_user -p"$sql_pass" users | tail -n +2); do
+	if [[ $(echo "SELECT is_premium FROM user WHERE username='$db_user';" | mysql --user=login_user -p"$sql_pass" users | tail -n +2) == 1 ]]; then
 		echo "user $db_user is premium"
 		if sudo usermod -G ssh_users "$db_user"; then
 			echo "[-] $(display_date) - user $db_user added to ssh_users group" |& tee -a "$log_file"
 		fi
-
 	else
 		echo "user $db_user is NOT premium"
-		if sudo deluser "$db_user" ssh_users; then
+		if sudo deluser "$db_user" ssh_users 2> /dev/null; then
 			echo "[-] $(display_date) - user $db_user removed from ssh_users group" |& tee -a "$log_file"
 		fi
 	fi
