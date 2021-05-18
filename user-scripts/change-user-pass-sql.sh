f9a2a1e73dcd05a0cf7bc63bdd3b2d188d4c2c67
luza-onarev <luza.onarev@protonmail.com>
2021-05-18 10:13:17 +0200

    Update change-user-pass-sql.sh

--- user-scripts/change-user-pass-sql.sh
+++ user-scripts/change-user-pass-sql.sh
@@ -4,7 +4,7 @@ display_date() {
 	date "+%H:%M:%S %d/%m/%Y"
 }
 
-sql_pass=$(sudo cat /etc/sql_login_user_pass)
+sql_pass=$(sudo cat /etc/sql_root_pass)
 
 while read -r line
 do
@@ -12,8 +12,9 @@ do
 	password=$(echo "$line" | awk '{print $2}')
 
 	if echo "$username:$password" | sudo chpasswd; then
+		echo "DELETE FROM user_acts WHERE username = '$username';" | mysql -u root -p"$sql_pass" users_actions
 		echo -e "Subject: == USER $username CHANGED PASSWORD ==" | sudo sendmail -f users-pass@mercury.cells.es ismael
 	else
 		echo -e "Subject: == USER $username CHANGED PASSWORD FAILED ==" | sudo sendmail -f users-pass@mercury.cells.es ismael
 	fi
-done < <(echo "select username,password from user_acts where action = 'cha'" | mysql -u login_user -p"$sql_pass" users_actions | tail -n +2)
+done < <(echo "select username,password from user_acts where action = 'cha'" | mysql -u root -p"$sql_pass" users_actions | tail -n +2)
