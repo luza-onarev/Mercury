31b0771cd45fd00d2db1af510295a145854754e2
luza-onarev <luza.onarev@protonmail.com>
2021-05-19 12:09:01 +0200

    Update ssh_users.sh

--- user-scripts/ssh_users.sh
+++ user-scripts/ssh_users.sh
@@ -6,10 +6,10 @@ display_date() {
 }
 
 log_file="/etc/user-scripts/log/ssh_users.log"
-sql_pass=$(sudo cat /etc/sql_login_user_pass)
+sql_pass=$(sudo cat /etc/sql_root_pass)
 
-for db_user in $(echo "SELECT username FROM user;" | mysql --user=login_user -p"$sql_pass" users | tail -n +2); do
-	if [[ $(echo "SELECT is_premium FROM user WHERE username='$db_user';" | mysql --user=login_user -p"$sql_pass" users | tail -n +2) == 1 ]]; then
+for db_user in $(echo "SELECT username FROM user;" | mysql -u root -p"$sql_pass" users | tail -n +2); do
+	if [[ $(echo "SELECT is_premium FROM user WHERE username='$db_user';" | mysql -u root -p"$sql_pass" users | tail -n +2) == 1 ]]; then
 		echo "user $db_user is premium"
 		if sudo usermod -G ssh_users "$db_user"; then
 			echo "[-] $(display_date) - user $db_user added to ssh_users group" |& tee -a "$log_file"
